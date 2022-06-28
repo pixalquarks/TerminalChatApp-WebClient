699a4cfe092ead0729ce35a2088d1598d5242990
pixalquarks <kashyaprakshat@gmail.com>
2022-06-28 20:45:55 +0530

    added dynamic module loading

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,17 @@
-import Header from "./Header";
-import UserList from "./UserList";
-import Chat from "./Chat";
-import Footer from "./Footer";
-import Login from "./Login";
-import Error from "./Error";
+import {lazy} from 'react';
 
-export { Header, UserList, Chat, Footer, Login, Error };
+// import Header from "./Header";
+// import UserList from "./UserList";
+// import Chat from "./Chat";
+// import Footer from "./Footer";
+// import Login from "./Login";
+// import Error from "./Error";
+
+const Header = lazy(() => import("./Header"));
+const UserList = lazy(() => import("./UserList"));
+const Chat = lazy(() => import("./Chat"));
+const Footer = lazy(() => import("./Footer"));
+const Login = lazy(() => import("./Login"));
+const Error = lazy(() => import("./Error"));
+
+export default { Header, UserList, Chat, Footer, Login, Error };
