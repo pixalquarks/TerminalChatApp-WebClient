9c65ebd599127077b78da5079da4f7022fadd889
pixalquarks <kashyaprakshat@gmail.com>
2022-06-28 20:40:43 +0530

    formatted and styled Error component

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import './Error.css';
+import React, { useContext } from "react";
+import "./Error.css";
+import { GRPCContext } from "../../context/GRPCContext";
+import { GRPCContextType } from "../../react-app-env";
 
 const Error = () => {
+  const { Retry, Relogin } = useContext(GRPCContext) as GRPCContextType;
+
+  const onRetry = async () => {
+    console.log("Retry Clicked");
+    await Retry();
+  };
+
   return (
     <>
-  <div id="error-box">
-    <div className="message"><h1 className="alert f-xl">Error!</h1><p>oh no, something went wrong.</p></div>
-    <button className="button-box first"><h1 className="red">Retry</h1></button>
-    <button className="button-box"><h1 className="red">Login</h1></button>
-  </div>
+      <div id="error-box">
+        <div className="message">
+          <h1 className="alert f-xl">Error!</h1>
+          <p>Disconnected from the server!!!</p>
+        </div>
+        <button className="button-box first" onClick={onRetry}>
+          <h1 className="red">Retry</h1>
+        </button>
+        <button className="button-box" onClick={Relogin}>
+          <h1 className="red">Login</h1>
+        </button>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Error 
+export default Error;
