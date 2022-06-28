5aafb4ca5d33fa6e45d6533b9035a0e34b5ed5ff
pixalquarks <kashyaprakshat@gmail.com>
2022-06-28 20:39:36 +0530

    now renders Error page when connections loses

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,33 +1,32 @@
-import React, {useState, useContext} from 'react';
-import './Login.css';
-import Address from '../Address';
-import Username from '../Username';
-import Error from '../Error';
-import { GRPCContext } from '../../context/GRPCContext';
-import { GRPCContextType } from '../../react-app-env';
+import React, { useState, useContext } from "react";
+import "./Login.css";
+import Address from "../Address";
+import Username from "../Username";
+import Error from "../Error";
+import { GRPCContext } from "../../context/GRPCContext";
+import { GRPCContextType } from "../../react-app-env";
 
 const Login = () => {
-
-  const {address, username} = useContext(GRPCContext) as GRPCContextType;
-  const [error, setError] = useState(true);
+  const { address, username, err } = useContext(GRPCContext) as GRPCContextType;
 
   return (
     <>
-    <div className="login blue-glassmorphism">
-    {error && <Error />}
-    {
-      !error &&
-      (address=="" || username == "") &&
-      <div className=''>
-          <h3 className="header-login">Login</h3>
-        <div className='card'>
-            { (address == "") && <Address />}
-            { !(address == "") && (username == "") && <Username/>}
+      {(err || address === "" || username === "") && (
+        <div className="login blue-glassmorphism">
+          {err && <Error />}
+          {!err && (address === "" || username === "") && (
+            <div className="">
+              <h3 className="header-login">Login</h3>
+              <div className="card">
+                {address === "" && <Address />}
+                {!(address === "") && username === "" && <Username />}
+              </div>
+            </div>
+          )}
         </div>
-      </div>}
-      </div>
-      </>
-      )
-}
+      )}
+    </>
+  );
+};
 
-export default Login
+export default Login;
